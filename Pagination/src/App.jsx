2125600637abdb55762f5a40ccfc86e8181c6074
Zacar <sakarmanandhar1@gmail.com>
2026-07-08 21:03:11 +0545

    backend approach

--- Pagination/src/App.jsx
+++ Pagination/src/App.jsx
@@ -4,24 +4,28 @@ import "./App.css";
 function App() {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+    );
     const data = await res.json();
 
     if (data && data.products) {
       setProducts(data.products);
+      setTotalPages(Math.ceil(data.total / 10));
     }
   };
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [page]);
 
   const selectPageHandler = (selectedPage) => {
     if (
       selectedPage >= 1 &&
-      selectedPage <= products.length / 10 &&
+      selectedPage <= totalPages &&
       selectedPage !== page
     )
       setPage(selectedPage);
@@ -31,7 +35,7 @@ function App() {
     <div className="App">
       {products.length > 0 && (
         <div className="products">
-          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+          {products.map((prod) => {
             return (
               <span className="products__single" key={prod.id}>
                 <img src={prod.thumbnail} alt={prod.title} />
@@ -51,7 +55,7 @@ function App() {
           >
             ⬅️
           </span>
-          {[...Array(products.length / 10)].map((_, i) => {
+          {[...Array(totalPages)].map((_, i) => {
             return (
               <span
                 className={page === i + 1 ? "pagination__selected" : ""}
@@ -68,9 +72,7 @@ function App() {
             onClick={() => {
               selectPageHandler(page + 1);
             }}
-            className={
-              page < products.length / 10 ? "" : "pagination__disabled"
-            }
+            className={page < totalPages ? "" : "pagination__disabled"}
           >
             ➡️
           </span>
