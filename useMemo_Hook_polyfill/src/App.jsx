c0c024d23af1130d03728df77b26a7f1ad0e17b4
Zacar <sakarmanandhar1@gmail.com>
2026-07-10 19:35:57 +0545

    useMemo Hook polyfill: useMemo hook example before making custom usememo hook

---
+++ useMemo_Hook_polyfill/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+
+import { useState, useMemo } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(100);
+  function increase() {
+    setCounter(counter + 1);
+  }
+
+  const squaredValue = () => {
+    console.log("Expensive Caluclation....");
+    return counter * counter;
+  };
+
+  const memoisedSquaredValue = useMemo(squaredValue, [counter]);
+
+  return (
+    <div className="App">
+      <h2>Counter : {counter}</h2>
+      <h2>Squared Counter: {memoisedSquaredValue}</h2>
+      <button onClick={increase}>Increment</button>
+      <h2>Counter : {counter2}</h2>
+      <button
+        onClick={() => {
+          setCounter2(counter2 - 1);
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default App;
