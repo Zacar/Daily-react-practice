e43061de6cd0881160ec7a258f651a1cadd5d7a0
Zacar <sakarmanandhar1@gmail.com>
2026-07-13 22:15:14 +0545

    Breadcrumbs : Routes for dummy ecommerce for breadcrumbs

---
+++ Breadcrumbs/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/home";
+import ProductListing from "./pages/product-listing";
+import ProductDetails from "./pages/product-details";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Sakar Store</h1>
+
+        {/* breadcrumbs  */}
+
+        {/* routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
