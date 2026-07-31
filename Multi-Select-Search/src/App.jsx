aa387374a39a317346b17d776f226c7945c76a77
Zacar <sakarmanandhar1@gmail.com>
2026-07-31 23:06:03 +0545

    Multi-Search-Select: Completed

--- Multi-Select-Search/src/App.jsx
+++ Multi-Select-Search/src/App.jsx
@@ -1,9 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import Pill from "./components/pill";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  const [selectedUsersSet, setSelectedUsersSet] = useState(new Set());
+
+  const inputRef = useRef(null);
 
   //https://dummyjson.com/users/search?q=John
 
@@ -26,26 +32,77 @@ function App() {
     fetchUsers();
   }, [searchTerm]);
 
+  const handleSelectUser = (user) => {
+    setSelectedUsers([...selectedUsers, user]);
+    setSelectedUsersSet(new Set([...selectedUsersSet, user.email]));
+    setSearchTerm("");
+    setSuggestions([]);
+    inputRef.current.focus();
+  };
+
+  const handleRemoveUser = (user) => {
+    const updatedUsers = selectedUsers.filter(
+      (selectedUsers) => selectedUsers.id !== user.id
+    );
+
+    setSelectedUsers(updatedUsers);
+
+    const updatedEmails = new Set(selectedUsersSet);
+    updatedEmails.delete(user.email);
+    setSelectedUsersSet(updatedEmails);
+  };
+
+  const handleKeyDown = (e) => {
+    if (
+      e.key == "Backspace" &&
+      e.target.value === "" &&
+      selectedUsers.length > 0
+    ) {
+      const lastUser = selectedUsers[selectedUsers.length - 1];
+      handleRemoveUser(lastUser);
+      setSuggestions("");
+    }
+  };
+
   return (
     <div className="user-search-container">
       <div className="user-search-input">
         {/* {pills} */}
 
+        {selectedUsers.map((user) => {
+          return (
+            <Pill
+              key={user.email}
+              image={user.image}
+              text={`${user.firstName} ${user.lastName}`}
+              onClick={() => {
+                handleRemoveUser(user);
+              }}
+            />
+          );
+        })}
         {/* inputfield with search suggestions */}
         <div>
           <input
+            ref={inputRef}
             type="text"
             value={searchTerm}
             onChange={(e) => {
               setSearchTerm(e.target.value);
             }}
             placeholder="Search for a User...."
+            onKeyDown={handleKeyDown}
           />
           {/* search suggestions */}
           <ul className="suggestions-list">
             {suggestions?.users?.map((user, index) => {
-              return (
-                <li key={user.email}>
+              return !selectedUsersSet.has(user.email) ? (
+                <li
+                  key={user.email}
+                  onClick={() => {
+                    handleSelectUser(user);
+                  }}
+                >
                   <img
                     src={user.image}
                     alt={`${user.firstName} ${user.lastName}`}
@@ -54,6 +111,8 @@ function App() {
                     {user.firstName} {user.lastName}
                   </span>
                 </li>
+              ) : (
+                <></>
               );
             })}
           </ul>
