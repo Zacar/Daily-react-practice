3e34ade045a9819c071b6f5136c013e9cc2a50c3
Zacar <sakarmanandhar1@gmail.com>
2026-08-08 22:06:01 +0545

    File-Explorer: first commit

---
+++ File-Explorer/src/components/Folder.jsx
@@ -0,0 +1,17 @@
+const Folder = ({ explorer }) => {
+  return (
+    <div style={{ marginTop: 5 }}>
+      <div>
+        <span>📁{explorer.name}</span>
+      </div>
+
+      <div>
+        {explorer.items.map((exp) => {
+          return <span>{exp.name}</span>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Folder;
