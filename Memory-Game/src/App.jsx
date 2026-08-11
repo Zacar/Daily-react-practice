5e6ad720522d96d3e13915afe5a19c5fa9cd8f42
Zacar <sakarmanandhar1@gmail.com>
2026-08-11 22:38:57 +0545

    Memory-Game: First Comment -  Basic layout

---
+++ Memory-Game/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import MemoryGame from "./components/memory-game";
+function App() {
+  return (
+    <div className="App">
+      <MemoryGame></MemoryGame>
+    </div>
+  );
+}
+
+export default App;
---
+++ Memory-Game/src/components/memory-game.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const MemoryGame = () => {
+  const [gridSize, setGridSize] = useState(4);
+  const [cards, setCards] = useState([]);
+
+  const [flipped, setFlipped] = useState([]);
+  const [solved, setSolved] = useState([]);
+  const [disabled, setDisabled] = useState(false);
+  const [won, setWon] = useState(false);
+
+  const handleGridSizeChange = (e) => {
+    const size = parseInt(e.target.value);
+    if (size >= 2 && size <= 10) setGridSize(size);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-grey-100 p-4">
+      <h1 className="text-3xl font-bold mb-6">Memory Game</h1>
+      {/* Input */}
+      <div className="mb-4">
+        <label htmlFor="gridSize" className="mr-2">
+          Grid Size: (max 10)
+        </label>
+        <input
+          type="number"
+          id="gridSize"
+          min="2"
+          max="10"
+          value={gridSize}
+          onChange={handleGridSizeChange}
+          className="border-2 border-gray-300 rounded px-2 py-1"
+        />
+      </div>
+      {/* Gmae board */}
+
+      {/* Result */}
+
+      {/* rESET/play again btn */}
+    </div>
+  );
+};
+
+export default MemoryGame;
