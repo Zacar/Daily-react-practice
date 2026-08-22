f5d5b90c0994b55ad2ead2456354334f4c4e81e1
Zacar <sakarmanandhar1@gmail.com>
2026-08-22 22:38:35 +0545

    I18Next: completed

--- I18Next/src/App.jsx
+++ I18Next/src/App.jsx
@@ -1,20 +1,35 @@
 import "./App.css";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import LanguageSelector from "./components/language-selector";
 
 function App() {
   const { t } = useTranslation();
 
   const { line1, line2 } = t("description", {
-    name: "XYZ",
+    name: "ram",
   });
 
   return (
     <div className="container">
       <LanguageSelector />
       <h1>{t("greeting")}</h1>
-      <p>{line1}</p>
-      <span>{line2}</span>
+      <Trans
+        // i18nKey={"description.line1"}
+        i18nKey={line1}
+        values={{
+          name: "ram",
+        }}
+        components={{ 1: <b /> }}
+      />
+      {/* <p>{line1}</p> */}
+      <Trans
+        // i18nKey={"description.line1"}
+        i18nKey={line2}
+        values={{
+          name: "ram",
+        }}
+        components={{ 1: <span /> }}
+      />
     </div>
   );
 }
--- I18Next/src/i18n.js
+++ I18Next/src/i18n.js
@@ -1,62 +1,20 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
+  .use(Backend)
   .init({
     debug: true,
     lng: "en",
     fallbackLng: "en",
     defaultNS: "translations",
-    returnObjects: true,
 
-    resources: {
-      en: {
-        translations: {
-          greeting: "Hello, Welcome!",
-          description: {
-            line1: "hello how are you",
-            line2: "I am {{name}} Manandhar your developer",
-          },
-        },
-      },
-      fr: {
-        translations: {
-          greeting: "Bonjour, Bienvenue !",
-          description: {
-            line1: "Je m'appelle {{name}}.",
-            line2: "Merci beaucoup !",
-          },
-        },
-      },
-      ne: {
-        translations: {
-          greeting: "नमस्कार, स्वागत छ!",
-          description: {
-            line1: "तपाईंलाई कस्तो छ?",
-            line2: "तपाईंलाई भेटेर खुसी लाग्यो।",
-          },
-        },
-      },
-      jp: {
-        translations: {
-          greeting: "こんにちは!",
-          description: {
-            line1: "元気ですか？",
-            line2: "私の名前はサカールです",
-          },
-        },
-      },
-      ar: {
-        translations: {
-          greeting: "مرحباً، أهلاً وسهلاً!",
-          description: {
-            line1: "مرحباً، كيف حالك؟",
-            line2: "أنا ساكار ماناندار، مطورك.",
-          },
-        },
-      },
+    returnObjects: true,
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json",
     },
   });
